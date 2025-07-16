6eb4dd4dd12be627606ee973227e346239ccf305
muhammed sabith <sabithhadi67@gmail.com>
2025-07-16 11:50:54 +0530

    completed js

--- ticket.js
+++ ticket.js
@@ -1,20 +1,21 @@
-var age = 10 ;
+var age = 25;
 
-var rate ;
-var category ;
+var rate;
+var category;
 
-if (age >= 0 && age <= 10) {
+if (age <=10){
     rate = 10;
     category = "Children";
 }
-else if (age >= 11 && age <= 59) {
+else if (age >= 10&& age <= 60){
     rate = 20;
     category = "adult";
 }
-else  (age >= 60) {
+else  {
     rate = 15;
     category = "grand parents";
 }
 
-console.log("Rate:", rate);
-console.log("Category:", category);
+
+console.log("rate:", rate);
+console.log("category:", category);
